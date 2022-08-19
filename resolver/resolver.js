b16ce4067cf5e4fee9c1183bcfbdf9b6b8674b38
matteo-cristino <cristino.matteo97@gmail.com>
2022-08-19 16:04:19 +0200

    feat(resolver): import host and http ports from .env

--- resolver/resolver.js
+++ resolver/resolver.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const axios = require('axios').default;
 
+dotenv.config();
+
+const RESOLVER_PORT = parseInt(process.env.RESOLVER_HTTP_PORT || "3000", 10);;
+const RESOLVER_HOST = process.env.RESOLVER_HOST || "0.0.0.0";
+const DID_PORT = parseInt(process.env.HTTP_PORT || "3000", 10);
+
 const app = express();
 
-const PORT = 12002;
-const HOST = "localhost";
-
 app.use('/1.0/identifiers/:did', function(req, res) {
-    axios.get('http://localhost:12001/api/W3C-DID-resolve-did?data=%7B%22id%22%3A%22'+req.params.did+'%22%7D')
+    axios.get('http://localhost:'+DID_PORT+'/api/W3C-DID-resolve-did?data=%7B%22id%22%3A%22'+req.params.did+'%22%7D')
 	.then(function (response) {
 	    res.send(response.data['W3C-DID'] || response.data);
 	});
 });
 
 // Start Proxy
-app.listen(PORT, HOST, () => {
+app.listen(RESOLVER_PORT, RESOLVER_HOST, () => {
     console.log(`Starting Proxy at ${HOST}:${PORT}`);
 });
