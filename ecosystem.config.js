8a7936866fc5a7e2f1947265ba31e3240646e6c5
matteo-cristino <cristino.matteo97@gmail.com>
2022-08-19 15:27:43 +0200

    pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps : [{
+        name : "did",
+        script : "./restroom/restroom.mjs"
+    },{
+        name : "resolver",
+        script: "./resolver/resolver.js"
+    }]
+}
