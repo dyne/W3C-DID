47b689fd17a82fa9f6adc450188fa8514c7e60b4
matteo-cristino <cristino.matteo97@gmail.com>
2022-08-22 08:36:26 +0200

    fix(resolver): resolver now use host from .env file

--- resolver/resolver.js
+++ resolver/resolver.js
@@ -7,11 +7,12 @@ dotenv.config({ path: `${__dirname}/../.env`});
 const RESOLVER_PORT = parseInt(process.env.RESOLVER_PORT || "3000", 10);;
 const RESOLVER_HOST = process.env.RESOLVER_HOST || "0.0.0.0";
 const DID_PORT = parseInt(process.env.HTTP_PORT || "3000", 10);
+const DID_HOST = process.env.HOST || "localhost";
 
 const app = express();
 
-app.use('/1.0/identifiers/:did', function(req, res) {
-    axios.get('http://localhost:'+DID_PORT+'/api/W3C-DID-resolve-did?data=%7B%22id%22%3A%22'+req.params.did+'%22%7D')
+app.get('/1.0/identifiers/:did', function(req, res) {
+    axios.get(`http://${DID_HOST}:${DID_PORT}/api/W3C-DID-resolve-did?data=%7B%22id%22%3A%22${req.params.did}%22%7D`)
 	.then(function (response) {
 	    res.send(response.data['W3C-DID'] || response.data);
 	});
