e737a08d240845117032d731658540add843a157
matteo-cristino <cristino.matteo97@gmail.com>
2022-08-24 12:25:39 +0200

    init now produce also the public keys

--- init.mjs
+++ init.mjs
@@ -10,17 +10,25 @@ dotenv.config();
 const PRIVATE_ZENCODE_DIR = process.env.PRIVATE_ZENCODE_DIR;
 const ZENCODE_DIR = process.env.ZENCODE_DIR || "contracts";
 
-// generate private keys
-const generatePrivateKeysScript = await fsp.readFile(path.join(PRIVATE_ZENCODE_DIR, "create_keys.zen"), 'utf8')
-let keyring = {};
-let keys;
-const params = {};
-try {
-    keys = await zencode_exec(generatePrivateKeysScript, params);
-} catch (e) {
-    console.log("Error from zencode_exec: ", e);
+const zen = async (zencode, keys, data) => {
+    const params = {};
+    if (keys !== undefined && keys !== null) {
+        params.keys = typeof keys === 'string' ? keys : JSON.stringify(keys);
+    }
+    if (data !== undefined && data !== null) {
+        params.data = typeof data === 'string' ? data : JSON.stringify(data);
+    }
+    try {
+        return await zencode_exec(zencode, params);
+    } catch (e) {
+        console.log("Error from zencode_exec: ", e);
+    }
 }
 
+// generate private keys
+const generatePrivateKeysScript = await fsp.readFile(path.join(PRIVATE_ZENCODE_DIR, "create_keys.zen"), 'utf8');
+let keyring = {};
+const keys = await zen(generatePrivateKeysScript, null, null);
 if (!keys) {
     console.error("Error in generate private keys");
     process.exit(-1)
@@ -30,3 +38,16 @@ Object.assign(keyring, JSON.parse(keys.result))
 await fsp.writeFile(
     path.join(ZENCODE_DIR, "keyring.json"),
     JSON.stringify(keyring), {mode: 0o600})
+
+const generatePublicKeysScript = await fsp.readFile(path.join(PRIVATE_ZENCODE_DIR, "create_pub_keys.zen"), 'utf8');
+let publicKeys = {};
+const pubKeys = await zen(generatePublicKeysScript, keyring, null);
+if (!pubKeys) {
+    console.error("Error in generate public keys");
+    process.exit(-1)
+}
+
+Object.assign(publicKeys, JSON.parse(pubKeys.result))
+await fsp.writeFile(
+    path.join(ZENCODE_DIR, "public_keys.json"),
+    JSON.stringify(publicKeys), {mode: 0o600})
