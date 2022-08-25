42f9e54e5af814e9439e1e08269d0c11ddca5425
Alberto Lerda <albertolerda97@gmail.com>
2022-08-25 15:46:51 +0200

    do not use npx

---
+++ restroom/restroom.mjs
@@ -0,0 +1,75 @@
+import fs from 'fs';
+import readdirp from 'readdirp';
+import express from "express";
+import chalk from "chalk";
+import bodyParser from "body-parser";
+import zencode from "@restroom-mw/core";
+import db from "@restroom-mw/db";
+import fabric from "@restroom-mw/fabric";
+import files from "@restroom-mw/files";
+import rrhttp from "@restroom-mw/http";
+import rrredis from "@restroom-mw/redis";
+import ethereum from "@restroom-mw/ethereum";
+import planetmint from "@restroom-mw/planetmint";
+import timestamp from "@restroom-mw/timestamp";
+import ui from "@restroom-mw/ui";
+import cors from "cors"
+
+import http from "http";
+import morgan from "morgan"
+import dotenv from "dotenv";
+dotenv.config();
+
+const HTTP_PORT = parseInt(process.env.HTTP_PORT || "3000", 10);
+const HOST = process.env.HOST || "0.0.0.0";
+const ZENCODE_DIR = process.env.ZENCODE_DIR;
+const OPENAPI = JSON.parse(process.env.OPENAPI || true);
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+app.set("json spaces", 2);
+
+app.use(cors())
+
+app.use(db.default);
+app.use(fabric.default);
+app.use(files.default);
+app.use(rrhttp.default);
+app.use(rrredis.default);
+app.use(ethereum.default);
+app.use(planetmint.default);
+app.use(timestamp.default);
+if (OPENAPI) {
+  app.use("/docs", ui.default({ path: ZENCODE_DIR }));
+}
+
+app.use("/api/*", zencode.default);
+
+const contracts = fs.readdirSync(ZENCODE_DIR);
+
+if (contracts.length > 0) {
+  const httpServer = http.createServer(app);
+  httpServer.listen(HTTP_PORT, HOST, () => {
+    console.log(`🚻 Restroom started on http://${chalk.bold.blue(HOST)}:${HTTP_PORT}`);
+    console.log(`📁 the ZENCODE directory is: ${chalk.magenta.underline(ZENCODE_DIR)} \n`);
+
+    if (OPENAPI) {
+      console.log(`To see the OpenApi interface head your browser to: ${chalk.bold.blue.underline('http://' + HOST + ':' + HTTP_PORT + '/docs')}`);
+      console.log(`To disable OpenApi, run ${chalk.bold('OPENAPI=0 yarn start')}`);
+    } else {
+      console.log(`⚠️ The OpenApi is not enabled! NO UI IS SERVED. To enable it run run ${chalk.bold('OPENAPI=1 yarn start')}`);
+    }
+
+    console.log("\nExposing");
+    readdirp(ZENCODE_DIR, { fileFilter: '*.zen|*.yaml|*.yml' }).on('data', (c) => {
+      const endpoint = `/api/${c.path.replace('.zen', '')}`
+      console.log(`\t${chalk.bold.green(endpoint)}`);
+    });
+  });
+} else {
+  console.log(`🚨 The ${chalk.magenta.underline(ZENCODE_DIR)} folder is empty, please add some ZENCODE smart contract before running Restroom`);
+}
+
