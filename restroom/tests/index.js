d2b1866c71b61c3d96c085400e4b42c8f9e110c6
matteo-cristino <cristino.matteo97@gmail.com>
2022-12-09 11:30:37 +0100

    fix: 🔍️ sandbox check public keys encoding before creating did-document

--- restroom/tests/index.js
+++ restroom/tests/index.js
@@ -23,7 +23,7 @@ test.serial("Create a simple did document and verify it", async (t) => {
 	"bitcoin_public_key": "24FWY6sMx2MvH1EEoncuWr4dh4NJ7Pmo5WDNst4oztg7s",
 	"ecdh_public_key": "SJ3uY8Y5cKYsMqqvW3rZaX7h4s1ms5NpAYeHUi16A7jHMVtwSF3Gdzafh9XmvGz6uNksBnaU5fvarDw1mZF2Nkjz",
 	"eddsa_public_key": "2s5wmQjZeYtpckyHakLiP5ujWKDL1M2b8CiP6vwajNrK",
-	"ethereum_address": "2d7VjFP8URbrYFqXYWkUNW9gbiaS",
+	"ethereum_address": "747846c15dfc79803265f953d003ac4251867cd7",
 	"reflow_public_key": "3LfL2v8qz2cmgy8LRqLPL4H12mt2rW3p7hrwJ6q1gqpHKyXWovkCutsJRsLxkrgHwQ233gouwWFmzshS5EnK9dah92855jzaqV4fD53svqLBrxdV2nt44aEMuWoXYSwA4dmTwHXpgsyQuCsn6uNewbF5VLcesqJubzHf4XvVF9249F1HVLmMR7oCKVBnCw3pTB2HrcmSJaSdKu88rJbzELTvdMLbXXyEcCvYDT3HhzGXNv9BBTo9ZXQGw1CSCCyDrCNMYe"
   }
 
