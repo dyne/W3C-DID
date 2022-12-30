9283f4d5009a297380e388d632efa32993bcdfe1
matteo-cristino <cristino.matteo97@gmail.com>
2022-12-30 15:44:02 +0100

    refactor(integrity): ♻️ reduce zencode statements

--- integrity_check/integrity_check.sh
+++ integrity_check/integrity_check.sh
@@ -13,7 +13,7 @@ check () {
                 | sed -e 's/:/\//g' -e 's/\./\//g' -e "s|^did/dyne|$path_to_data|g"`
             jq '{"signer_data": .}' <$signer_did_path >/tmp/signer.json
             jq '{"check_data": .}' <$pathname >/tmp/check.json
-            zenroom -a /tmp/signer.json -k /tmp/check.json -z integrity_check.zen 2>/dev/null 1>/dev/null
+            ./zenroom -a /tmp/signer.json -k /tmp/check.json -z integrity_check.zen 2>/dev/null 1>/dev/null
             if [[ $? != 0 ]]; then echo -e "\e[0;31mDid document not valid: ${pathname}\e[0m"; fi
         fi
     done
