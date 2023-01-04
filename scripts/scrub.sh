ce56b454f20ac26f1e43f804571992b51489c02b
Jaromil <jaromil@dyne.org>
2023-01-04 06:25:00 +0100

    fixes to scrub

--- scripts/scrub.sh
+++ scripts/scrub.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
 # runtime checks
-command -d zenroom > /dev/null || {
+command -v zenroom > /dev/null || {
 	>&2 echo "Zenroom executable missing"
-    exit 1 }
-[ "$1" == "" ] && { >&2 echo "$0 path/to/data [spec]"; exit 1 }
-[ -d "$1" ]    || { >&2 echo "Invalid directory: $1"; exit 1 }
+    exit 1
+}
+[ "$1" == "" ] && { >&2 echo "$0 path/to/data [spec]"; exit 1;}
+[ -d "$1" ]    || { >&2 echo "Invalid directory: $1"; exit 1;}
 [ "$2" == "" ] || {
-	[ -d "$1/$2" ] || { >&2 echo "Invalid spec: $1/$2"; exit 1 }
+	[ -d "$1/$2" ] || { >&2 echo "Invalid spec: $1/$2"; exit 1;}
 }
 
 data="$1"
 spec="$2"
 log=scrub.log
 # TODO: scrub-`date`.log to archive multiple logs
-echo "-- \nStarting scrub on `date`" | >&2 tee -a $log
+echo -e "-- \nStarting scrub on `date`" | >&2 tee -a $log
 
 # takes one argument: spec directory path
 scrub() {
-	[ -d "$1"/"$2" ] || {
-		echo "scrub() :: Invalid directory: $1/$2" | >&2 tee -a $log
-		return 1 }
-    for pathname in "$1"/"$2"/*; do
+	[ -d "$1" ] || {
+		echo "scrub() :: Invalid directory: $1" | >&2 tee -a $log
+		return 1
+	}
+    for pathname in "$1"/* ; do
+		# one level specs only
+		[ -d "$pathname" ] && continue
+		# >&2 echo "check $pathname"
+
 		tempsigner=`mktemp`
 		tempdid=`mktemp`
         signer_did_path=`\
-			        jq -r '.didDocument.proof.verificationMethod' $pathname \
-            		    | cut -d "#" -f 1 \
-                | se	d -e 's/:/\//g' -e 's/\./\//g' -e "s|^did/dyne|$initial_path|g"`
+		  jq -r '.didDocument.proof.verificationMethod' $pathname \
+          | cut -d "#" -f 1 \
+          | sed -e 's/:/\//g' -e 's/\./\//g' -e "s|^did/dyne|$1|g"`
+		# >&2 echo "$signer_did_path"
 		[ -f "signer_did_path" ] || {
-			echo "scrub() :: Signer did not found: $signer_did_path" | >&2 tee -a $log
-			return 1 }
+			echo "scrub(`basename $1`) :: Signer did not found for: `basename $pathname`" | >&2 tee -a $log
+			return 1;}
         jq '{"signer_data": .}' <${signer_did_path} >${tempsigner}
         jq '{"check_data": .}' <${pathname} >${tempdid}
 		>&2 echo -n "`basename $pathname` - "
@@ -44,14 +51,18 @@ and I rename 'didDocument' to 'check_did_document'
 When I verify the did document named 'check_did_document' is signed by 'signer_did_document'
 Then print the string 'OK'
 EOF
-         [ $? == 0 ] || {
-			 echo "Invalid DID signature: ${pathname}" | >&2 tee -a $log
-		 }
+        [ $? == 0 ] || {
+			echo "Invalid DID signature: ${pathname}" | >&2 tee -a $log
+		}
+		rm -f ${tempsigner} ${tempdid}
     done
 }
 
 if [ "$spec" == "" ]; then
-	find "$data"/ -type -d -exec scrub {} \;
+	for dir in "$data"/*; do
+		[ -d "$dir" ] || continue
+		scrub $dir
+	done
 else
 	scrub "$data"/"$spec"
 fi
