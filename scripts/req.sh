51e2a2289272b1183493b3a3971304aa163a222d
matteo-cristino <cristino.matteo97@gmail.com>
2023-01-05 17:15:55 +0100

    refactor(scripts): remove extras input since unused

--- scripts/req.sh
+++ scripts/req.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 
-# example: ./req.sh ifacer.A "{\"ifacer_id\":{}}"
+# example: ./req.sh ifacer.A
 command -v zenroom > /dev/null || {
 	>&2 echo "Zenroom executable missing"; exit 1
 }
-[ "$1" == "" ] && { >&2 echo "$0 spec [extras]"; exit 1;}
-#[ "$2" != "" ] && [ -f $2 ] || { >&2 echo "extra argument is not a path"; exit 1;}
+[ "$1" == "" ] && { >&2 echo "$0 spec"; exit 1;}
+
+contracts=client/v1
 
 # different specs can have different did-settings
 case $1 in
     ifacer*)
         did_settings=client/v1/ifacer/did-settings.json
-        contracts=client/v1/ifacer
     ;;
     *)
         did_settings=client/v1/did-settings.json
-        contracts=clinet/v1
     ;;
 esac
 
 # generate pks
 tmppk=`mktemp`
 zenroom -z -k keyring.json -a ${did_settings} \
-        client/v1/create-identity-pubkeys.zen > ${tmppk}
+        ${contracts}/create-identity-pubkeys.zen > ${tmppk}
 
 # set did_spec and extras if present
 tmp=`mktemp` && jq --arg value $1 '.did_spec = $value' ${tmppk} > ${tmp} && mv ${tmp} ${tmppk}
-if [ "$2" != "" ]; then
-    tmpextras=`mktemp`
-    echo $2 > ${tmpextras}
-    tmp=`mktemp` && jq -s '.[0] * .[1]' ${tmppk} ${tmpextras} > ${tmp} && mv ${tmp} ${tmppk}
-    rm -f ${tmpextras}
-fi
-cat ${tmppk} | jq .
 
 # create did doc
 zenroom -z -a ${tmppk} \
 		${contracts}/pubkeys-request-unsigned.zen > did_doc.json
+
+rm -f ${tmppk}
