23098076ce1cf72947dd7f7374b4b16e7b49d81d
matteo-cristino <cristino.matteo97@gmail.com>
2023-01-09 12:03:18 +0100

    test: 🧪 update restroom tests

--- test/run.sh
+++ test/run.sh
@@ -1,59 +1,116 @@
 #!/bin/bash
+RR_PORT=3000
+zenroom_res="^==== Zenroom result ===="
 
-echo "CLIENT SIDE"
+echo "SANDBOX CLIENT SIDE"
 echo "- generating controller keyring"
-./zenroom -z client/v1/sandbox/sandbox-keygen.zen \
-		-a client/v1/did-settings.json \
-		> /tmp/controller-keyring.json 2>/tmp/controller-keyring-err.json
-if [[ $? != 0 ]]; then
-  echo "Generating controller keyring failed with error: "
-  cat /tmp/controller-keyring-err.json
-  exit 1
-else
-  rm /tmp/controller-keyring-err.json
-fi
+tmpctrlkey=`mktemp`
+tmpctrlkey_err=`mktemp`
+./zenroom -z -a client/v1/did-settings.json \
+    client/v1/sandbox/sandbox-keygen.zen \
+    > ${tmpctrlkey} 2>${tmpctrlkey_err}
+[ "$?" != "0" ] && {
+    echo "Generating controller keyring failed with error: "
+    cat ${tmpctrlkey_err}
+    exit 1
+}
+rm -f ${tmpctrlkey_err}
 
 echo "- generating pubkeys and identity"
-./zenroom -z client/v1/sandbox/create-identity-pubkeys.zen \
-		> /tmp/new-id-pubkeys.json 2>/tmp/new-id-pubkeys-err.json
-if [[ $? != 0 ]]; then
-  echo "Generating pubkeys and identity failed with error: "
-  cat /tmp/new-id-pubkeys-err.json
-  exit 1
-else
-  rm /tmp/new-id-pubkeys-err.json
-fi
+tmppk=`mktemp`
+tmppk_err=`mktemp`
+    ./zenroom -z client/v1/sandbox/create-identity-pubkeys.zen \
+    > ${tmppk} 2>${tmppk_err}
+[ "$?" != "0" ] && {
+    echo "Generating pubkeys and identity failed with error: "
+    cat ${tmppk_err}
+    exit 1
+}
+rm -f ${tmppk_err}
 
 echo "- generating request"
-jq --arg value $(($(date +%s%N)/1000000)) '.timestamp = $value' /tmp/new-id-pubkeys.json \
-        > /tmp/new-id-pubkeys-tmp.json && mv /tmp/new-id-pubkeys-tmp.json /tmp/new-id-pubkeys.json
-./zenroom -z client/v1/sandbox/pubkeys-request.zen \
-		-a /tmp/new-id-pubkeys.json -k /tmp/controller-keyring.json \
-		> /tmp/pubkeys-request.json 2>/tmp/pubkeys-request-err.json
-if [[ $? != 0 ]]; then
-  echo "Generating request failed with error: "
-  cat /tmp/pubkeys-request-err.json
-  exit 1
-else
-  rm /tmp/pubkeys-request-err.json
-fi
+tmpreq=`mktemp`
+tmpreq_err=`mktemp`
+tmp=`mktemp` && jq --arg value $(($(date +%s%N)/1000000)) '.timestamp = $value' ${tmppk} > ${tmp} && mv ${tmp} ${tmppk}
+./zenroom -z -a ${tmppk} -k ${tmpctrlkey} \
+    client/v1/sandbox/pubkeys-request.zen \
+    > ${tmpreq} 2>${tmpreq_err}
+[ "$?" != "0" ] && {
+    echo "Generating request failed with error: "
+    cat ${tmpreq_err}
+    exit 1
+}
+rm -f ${tmpreq_err}
+
+
+echo "- generating update request"
+tmpupd=`mktemp`
+tmpupd_err=`mktemp`
+tmp=`mktemp` && jq --arg value $(($(date +%s%N)/1000000)) '.timestamp |= $value' ${tmppk} > ${tmp} && mv ${tmp} ${tmppk}
+new_ecdh=`./zenroom -z client/v1/sandbox/create-identity-pubkeys.zen 2>/dev/null | jq -r '.ecdh_public_key'`
+tmp=`mktemp` && jq --arg key $new_ecdh '.ecdh_public_key |= $key' ${tmppk} > ${tmp} && mv ${tmp} ${tmppk}
+./zenroom -z -a ${tmppk} -k ${tmpctrlkey} \
+    client/v1/sandbox/pubkeys-request.zen \
+    > ${tmpupd} 2>${tmpupd_err}
+[ "$?" != "0" ] && {
+    echo "Generating update request failed with error: "
+    cat ${tmpupd_err}
+    exit 1
+}
+rm -f ${tmpupd_err}
+
+echo "- generating deactivation request"
+tmpdct=`mktemp`
+tmpdct_err=`mktemp`
+./zenroom -z -a ${tmppk} -k ${tmpctrlkey} \
+    client/v1/sandbox/pubkeys-deactivate.zen \
+    > ${tmpdct} 2>${tmpdct_err}
+[ "$?" != "0" ] && {
+    echo "Generating deactivation request failed with error: "
+    cat ${tmpdct_err}
+    exit 1
+}
+rm -f ${tmpdct_err}
+
+rm -f ${tmppk} ${tmpctrlkey}
 
 echo "SERVER SIDE"
 echo "- accept request"
-curl -s -w "%{stderr}%{http_code}\n" -o /tmp/pubkeys-accept.json -X 'POST' \
-  'http://localhost:3000/api/v1/sandbox/pubkeys-accept.chain' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d "{
-  \"data\": $(cat /tmp/pubkeys-request.json),
-  \"keys\": {}
-}" 2>/tmp/status.json
-if [[ `cat /tmp/status.json` != "200" ]]; then
-  echo "Accepting requests failed with error: `cat /tmp/status.json`"
-  echo "and error:"
-  cat /tmp/pubkeys-accept.json
-  echo ""
-  exit 1
-else
-  rm /tmp/pubkeys-accept.json /tmp/status.json
-fi
+tmpres=`mktemp`
+./restroom-test -p ${RR_PORT} -u v1/sandbox/pubkeys-accept.chain -a ${tmpreq} > ${tmpres} 2>/dev/null
+[ ! -z "$(grep "$zenroom_res" "$tmpres")" ] && {
+    echo "Accepting request failed with error:" >&2
+    cat ${tmpres} >&2
+    exit 1
+}
+rm -f ${tmpres}
+
+echo "- does not accpet the same request twice"
+tmpres_fail=`mktemp`
+./restroom-test -p ${RR_PORT} -u v1/sandbox/pubkeys-accept.chain -a ${tmpreq} > ${tmpres_fail} 2>/dev/null
+[ -z "$(grep "$zenroom_res" "$tmpres_fail")" ] && {
+    echo "Accepting request succeeded, but it should not. Response:" >&2
+    cat ${tmpres_fail} >&2
+    exit 1
+}
+rm -f ${tmpres_fail} ${tmpreq}
+
+echo "- accept update request"
+tmpupd_res=`mktemp`
+./restroom-test -p ${RR_PORT} -u v1/sandbox/pubkeys-update.chain -a ${tmpupd} > ${tmpupd_res} 2>/dev/null
+[ ! -z "$(grep "$zenroom_res" "$tmpupd_res")" ] && {
+    echo "Accepting update request failed with error:" >&2
+    cat ${tmpupd_res} >&2
+    exit 1
+}
+rm -f ${tmpupd_res} ${tmpupd}
+
+echo "- accept deactivate request"
+tmpdct_res=`mktemp`
+./restroom-test -p ${RR_PORT} -u v1/sandbox/pubkeys-deactivate.chain -a ${tmpdct} > ${tmpdct_res} 2>/dev/null
+[ ! -z "$(grep "$zenroom_res" "$tmpdct_res")" ] && {
+    echo "Accepting deactivate request failed with error:" >&2
+    cat ${tmpdct_res} >&2
+    exit 1
+}
+rm -f ${tmpdct_res} ${tmpdct}
