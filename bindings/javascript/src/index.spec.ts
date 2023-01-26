70771a0ceaa0e75e79a317c5f0a8c312ccd923b2
matteo-cristino <cristino.matteo97@gmail.com>
2023-01-26 10:16:04 +0100

    fix(bindings): ♻️ better separation of partecipant and admin
    createRequest creates an unsigned request that will be signed from the admin with signRequest

--- bindings/javascript/src/index.spec.ts
+++ bindings/javascript/src/index.spec.ts
@@ -1,13 +1,15 @@
 import test from "ava";
 import * as dotenv from "dotenv";
 import { createKeyring, createRequest, createIfacerRequest,
-  deactivateRequest, sendRequest
+  deactivateRequest, sendRequest, signRequest
 } from "./index";
 dotenv.config();
 
-const sandboxTestAKeyring = process.env.TEST_ADMIN_KEYRING;
+const sandboxTestAKeyring = process.env.SANDBOX_TEST_ADMIN_KEYRING;
 let acceptRequest: string = null;
+let acceptSignedRequest: string = null;
 let updateRequest: string = null;
+let updateSignedRequest: string = null;
 let deleteRequest: string = null;
 let sandboxTestKeyring: string = null;
 let ifacerTestKeyring: string = null;
@@ -25,12 +27,21 @@ test.serial("Create a sandbox keyring", async (t) => {
   t.is(typeof r["sandbox_test_from_js"]["keyring"]["reflow"], "string");
 })
 
-test.serial("Create a sandbox accept request", async (t) => {
-  acceptRequest = await createRequest(
-    sandboxTestKeyring, "sandbox.test",
-    sandboxTestAKeyring, "sandbox.test_A");
+test.serial("Create an unsigned sandbox request", async (t) => {
+  acceptRequest = await createRequest(sandboxTestKeyring, "sandbox.test");
   t.is(typeof acceptRequest, "string");
   const r = JSON.parse(acceptRequest);
+  t.is(typeof r["did_document"]["@context"], "object");
+  t.is(typeof r["did_document"]["id"], "string");
+  t.is(typeof r["did_document"]["verificationMethod"], "object");
+  t.is(r["did_document"]["description"], "sandbox_test_from_js");
+})
+
+test.serial("Sign the sandbox accept request", async (t) => {
+  acceptSignedRequest = await signRequest(
+    acceptRequest, sandboxTestAKeyring, "sandbox.test_A");
+  t.is(typeof acceptSignedRequest, "string");
+  const r = JSON.parse(acceptSignedRequest);
   t.is(typeof r["did_document"]["@context"], "object");
   t.is(typeof r["did_document"]["id"], "string");
   t.is(typeof r["did_document"]["verificationMethod"], "object");
@@ -41,17 +52,40 @@ test.serial("Create a sandbox accept request", async (t) => {
   t.is(typeof r["id"], "string");
 })
 
-test.serial("Create an update request", async (t) => {
+test.serial("Send the create request", async (t) => {
+  const result = await sendRequest(
+    "api/v1/sandbox/pubkeys-accept.chain",
+    acceptSignedRequest
+    );
+  t.is(typeof result, "object");
+  t.is(typeof result["result"], "object");
+  t.is(typeof result["result"]["@context"], "string");
+  t.is(typeof result["result"]["didDocument"], "object");
+  t.is(typeof result["result"]["didDocumentMetadata"], "object");
+  t.is(typeof result["result"]["didDocumentMetadata"]["created"], "string");
+  t.is(result["result"]["didDocumentMetadata"]["deactivated"], "false");
+})
+
+test.serial("Create an unsigned update request", async (t) => {
   let updateDescription = JSON.parse(sandboxTestKeyring)
   updateDescription["controller"] = "sandbox_test_from_js_updated";
   updateDescription["sandbox_test_from_js_updated"] = updateDescription["sandbox_test_from_js"];
   delete updateDescription["sandbox_test_from_js"];
   sandboxTestKeyring = JSON.stringify(updateDescription);
-  updateRequest = await createRequest(
-    sandboxTestKeyring, "sandbox.test",
-    sandboxTestAKeyring, "sandbox.test_A");
+  updateRequest = await createRequest(sandboxTestKeyring, "sandbox.test");
   t.is(typeof updateRequest, "string");
   const r = JSON.parse(updateRequest);
+  t.is(typeof r["did_document"]["@context"], "object");
+  t.is(typeof r["did_document"]["id"], "string");
+  t.is(typeof r["did_document"]["verificationMethod"], "object");
+  t.is(r["did_document"]["description"], "sandbox_test_from_js_updated");
+})
+
+test.serial("Sign the sandbox update request", async (t) => {
+  updateSignedRequest = await signRequest(
+    updateRequest, sandboxTestAKeyring, "sandbox.test_A");
+  t.is(typeof updateSignedRequest, "string");
+  const r = JSON.parse(updateSignedRequest);
   t.is(typeof r["did_document"]["@context"], "object");
   t.is(typeof r["did_document"]["id"], "string");
   t.is(typeof r["did_document"]["verificationMethod"], "object");
@@ -60,6 +94,20 @@ test.serial("Create an update request", async (t) => {
   t.is(typeof r["eddsa_signature"], "string");
   t.is(typeof r["timestamp"], "string");
   t.is(typeof r["id"], "string");
+})
+
+test.serial("Send the update request", async (t) => {
+  const result = await sendRequest(
+    "api/v1/sandbox/pubkeys-update.chain",
+    updateSignedRequest
+    );
+  t.is(typeof result, "object");
+  t.is(typeof result["result"], "object");
+  t.is(typeof result["result"]["@context"], "string");
+  t.is(typeof result["result"]["didDocument"], "object");
+  t.is(typeof result["result"]["didDocumentMetadata"], "object");
+  t.is(typeof result["result"]["didDocumentMetadata"]["updated"], "string");
+  t.is(result["result"]["didDocumentMetadata"]["deactivated"], "false");
 })
 
 test.serial("Create a delete request", async (t) => {
@@ -71,34 +119,6 @@ test.serial("Create a delete request", async (t) => {
   t.is(typeof r["deactivate_id"], "string");
   t.is(typeof r["ecdh_signature"], "object");
   t.is(typeof r["id"], "string");
-})
-
-test.serial("Send the create request", async (t) => {
-  const result = await sendRequest(
-    "api/v1/sandbox/pubkeys-accept.chain",
-    acceptRequest
-    );
-  t.is(typeof result, "object");
-  t.is(typeof result["result"], "object");
-  t.is(typeof result["result"]["@context"], "string");
-  t.is(typeof result["result"]["didDocument"], "object");
-  t.is(typeof result["result"]["didDocumentMetadata"], "object");
-  t.is(typeof result["result"]["didDocumentMetadata"]["created"], "string");
-  t.is(result["result"]["didDocumentMetadata"]["deactivated"], "false");
-})
-
-test.serial("Send the update request", async (t) => {
-  const result = await sendRequest(
-    "api/v1/sandbox/pubkeys-update.chain",
-    updateRequest
-    );
-  t.is(typeof result, "object");
-  t.is(typeof result["result"], "object");
-  t.is(typeof result["result"]["@context"], "string");
-  t.is(typeof result["result"]["didDocument"], "object");
-  t.is(typeof result["result"]["didDocumentMetadata"], "object");
-  t.is(typeof result["result"]["didDocumentMetadata"]["updated"], "string");
-  t.is(result["result"]["didDocumentMetadata"]["deactivated"], "false");
 })
 
 test.serial("Send the delete request", async (t) => {
@@ -127,13 +147,9 @@ test.serial("Create a ifacer keyring", async (t) => {
   t.is(typeof r["ifacer_test_from_js"]["keyring"]["reflow"], "string");
 })
 
-test.serial("Create a ifacer accept request", async (t) => {
-  // the admin keyring is wrong, thus the accept request will fail if sent
-  // we do not send it, but just check that the identifier is present in the 
-  // did document
+test.serial("Create a ifacer unsigned request", async (t) => {
   const ifacerAcceptRequest = await createIfacerRequest(
-    ifacerTestKeyring, "ifacer.test", "061FKNW40X4CAEEAFSW8NZRCWG",
-    sandboxTestAKeyring, "ifacer.test_A");
+    ifacerTestKeyring, "ifacer.test", "061FKNW40X4CAEEAFSW8NZRCWG");
   t.is(typeof ifacerAcceptRequest, "string");
   const r = JSON.parse(ifacerAcceptRequest);
   t.is(typeof r["did_document"]["@context"], "object");
@@ -142,8 +158,4 @@ test.serial("Create a ifacer accept request", async (t) => {
   t.is(typeof r["did_document"]["verificationMethod"], "object");
   t.is(r["did_document"]["identifier"], "061FKNW40X4CAEEAFSW8NZRCWG");
   t.is(r["did_document"]["description"], "ifacer_test_from_js");
-  t.is(typeof r["ecdh_signature"], "object");
-  t.is(typeof r["eddsa_signature"], "string");
-  t.is(typeof r["timestamp"], "string");
-  t.is(typeof r["id"], "string");
 })
--- bindings/javascript/src/index.ts
+++ bindings/javascript/src/index.ts
@@ -9,6 +9,12 @@ const readFromFile = (
   return readFileSync(join(__dirname, pathToFile), 'utf-8');
 }
 
+/**
+ * create a keyring that contains the ecdh, eddsa, reflow, bitcoin and ethereum keys
+ * @param description description that will appear in the did document
+ * @returns JSON encoded as string with format
+ * {"controller": "`description`", "`description`": {"keyring": { *...* }}}
+ */
 export const createKeyring = async (
     description: string
 ) :Promise<string> => {
@@ -21,7 +27,6 @@ export const createKeyring = async (
 const preparePks = async (
   requestKeyring: string,
   requestDomain: string,
-  signerDomain: string,
   additionalData: string
 ) :Promise<string> => {
   const contractPks = readFromFile('client/v1/create-identity-pubkeys.zen');
@@ -29,39 +34,75 @@ const preparePks = async (
   let {result} = await zencode_exec(contractPks, {data, keys : requestKeyring});
   result = JSON.parse(result);
   result["did_spec"] = requestDomain;
-  result["signer_did_spec"] = signerDomain;
-  result["timestamp"] = new Date().getTime().toString();
   return JSON.stringify(result);
 }
 
+/**
+ * create an unsigned request containig the did document of the type did:dyne:`requestDomain`
+ * @param requestKeyring the keyring corresponding to the did document that will be created
+ * @param requestDomain the did domain of the request
+ * @returns JSON encoded as string contating the unsigned did document
+ */
 export const createRequest = async (
   requestKeyring: string,
-  requestDomain: string,
-  signerKeyring: string,
-  signerDomain: string
+  requestDomain: string
 ) :Promise<string> => {
-  const contractRequest = readFromFile('client/v1/pubkeys-request-signed.zen');
-  const data = await preparePks(requestKeyring, requestDomain, signerDomain, 'client/v1/did-settings.json');
-  const {result} = await zencode_exec(contractRequest, {data, keys : signerKeyring});
+  const contractRequest = readFromFile('client/v1/pubkeys-request-unsigned.zen');
+  const data = await preparePks(requestKeyring, requestDomain, 'client/v1/did-settings.json');
+  const {result} = await zencode_exec(contractRequest, {data, keys : "{}"});
   return result;
 }
 
+/**
+ * create an unsigned request containig the did document of the type did:dyne:`requestDomain`
+ * @param requestKeyring the keyring corresponding to the did document that will be created/updated
+ * @param requestDomain the did domain of the request
+ * @param requestIdentifier the ifacer identifier
+ * @returns JSON encoded as string contating the unsigned did document
+ */
 export const createIfacerRequest = async (
   requestKeyring: string,
   requestDomain: string,
-  requestIdentifier: string,
-  signerKeyring: string,
-  signerDomain: string
+  requestIdentifier: string
 ) :Promise<string> => {
-  const contractRequest = readFromFile('client/v1/ifacer/pubkeys-request-signed.zen');
-  let data = await preparePks(requestKeyring, requestDomain, signerDomain, 'client/v1/ifacer/did-settings.json');
+  const contractRequest = readFromFile('client/v1/ifacer/pubkeys-request-unsigned.zen');
+  let data = await preparePks(requestKeyring, requestDomain, 'client/v1/ifacer/did-settings.json');
   data = JSON.parse(data);
   data["identifier"] = requestIdentifier;
   data = JSON.stringify(data);
-  const {result} = await zencode_exec(contractRequest, {data, keys : signerKeyring});
+  const {result} = await zencode_exec(contractRequest, {data, keys : "{}"});
   return result;
 }
 
+/**
+ * sign with both the eddsa and the ecdh keys the request
+ * @param request the request to be signed
+ * @param signerKeyring the keyring of the signer
+ * @param signerDomain the did domain of the signer
+ * @returns signed request ready to be sent
+ */
+export const signRequest = async (
+  request: string,
+  signerKeyring: string,
+  signerDomain: string
+) :Promise<string> => {
+  const contractSign = readFromFile('client/v1/pubkeys-sign.zen');
+  request = JSON.parse(request);
+  request["timestamp"] = new Date().getTime().toString();
+  request["signer_did_spec"] = signerDomain;
+  request = JSON.stringify(request);
+  const {result} = await zencode_exec(contractSign, {data: request, keys : signerKeyring});
+  return result;
+}
+
+/**
+ * create and sign a did revocation request
+ * @param requestKeyring the keyring corresponding to the did document that will be deactivated
+ * @param requestDomain the did domain of the request
+ * @param signerKeyring the keyring of the signer
+ * @param signerDomain the did domain of the signer
+ * @returns signed revokation request ready to be sent
+ */
 export const deactivateRequest = async (
   requestKeyring: string,
   requestDomain: string,
@@ -69,11 +110,20 @@ export const deactivateRequest = async (
   signerDomain: string
 ) :Promise<string> => {
   const contractRequest = readFromFile('client/v1/pubkeys-deactivate.zen');
-  const data = await preparePks(requestKeyring, requestDomain, signerDomain, 'client/v1/did-settings.json');
+  let data = await preparePks(requestKeyring, requestDomain, 'client/v1/did-settings.json');
+  data = JSON.parse(data);
+  data["signer_did_spec"] = signerDomain;
+  data = JSON.stringify(data);
   const {result} = await zencode_exec(contractRequest, {data, keys : signerKeyring});
   return result;
 }
 
+/**
+ * send the `request` to https://did.dyne.org/`endpoint`
+ * @param endpoint api endpoint to send the request to
+ * @param request the signed request to be sent
+ * @returns the result of the request
+ */
 export const sendRequest = async (
   endpoint: string,
   request: string
