6e5e4a07c3499b18cdc551b9bda45aa2e7d05f6c
Alberto Lerda <albertolerda97@gmail.com>
2023-03-20 17:01:23 +0100

    feat: cron that ping uptime

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -1,27 +1,42 @@
 #!/bin/bash
 set -e
 
-[ "$1" = "" ]                && { >&2 echo "$0 path/to/data"; exit 1;}
-[ "`basename $1`" = "data" ] || { >&2 echo "$0 path/to/data"; exit 1;}
-[ -d "$1" ]                  || { >&2 echo "Invalid directory: $1"; exit 1;}
+data_dir="$1"
+
+[ "$data_dir" = "" ]                && { >&2 echo "$0 path/to/data"; exit 1;}
+[ "`basename $data_dir`" = "data" ] || { >&2 echo "$0 path/to/data"; exit 1;}
+[ -d "$data_dir" ]                  || { >&2 echo "Invalid directory: $1"; exit 1;}
+
 
 # copy all committed files (not pushed) in data/dyne/git_check
-cd $1
-committed_files=`git diff --name-only origin/main HEAD`
-sandbox_files=`echo $committed_files | grep "dyne/sandbox"`
-[ "$sandbox_files" = "" ] || {
-    >&2 echo "Sandbox files found in committed files: "
-    echo $sandbox_files | tr ' ' '\n' >&2
-    exit 1
-}
-mkdir dyne/git_check
-cp $committed_files dyne/git_check
-cd -
 
-# scrub data/dyne/git_check
-./scripts/scrub.sh $1/dyne git_check
-[ $? != "0" ] && { rm -r $1/dyne/git_check; exit $?;}
-rm -r $1/dyne/git_check
+committed_files=`cd "$data_dir"; git diff --name-only origin/main HEAD`
+if [ "$committed_files" = "" ]; then
+    >&2 echo "No committed files to be checked"
+else
+    sandbox_files=`cd $data_dir; echo $committed_files | grep "dyne/sandbox" || true`
+    [ "$sandbox_files" = "" ] || {
+        >&2 echo "Sandbox files found in committed files: "
+        echo $sandbox_files | tr ' ' '\n' >&2
+        exit 1
+    }
+    cd "$data_dir"
+    rm -rf dyne/git_check
+    mkdir dyne/git_check
+    cp $committed_files dyne/git_check
+    cd -
+
+    # scrub data/dyne/git_check
+    set +e
+    ./scripts/scrub.sh $1/dyne git_check
+    if [ $? != "0" ]; then
+        rm -r "$data_dir/dyne/git_check"
+        exit $?
+    fi
+    rm -r "$data_dir/dyne/git_check"
+    set -e
+fi
 
 # push
-cd $1 && git push
+cd $data_dir && git push
+curl "https://uptime.dyne.org/api/push/SZJSAEl66T?status=up&msg=OK&ping="
