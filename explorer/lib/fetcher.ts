41413fea9dae778d072e285c36e6363934422d2c
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-30 13:58:15 +0100

    feat: Add DID detail error (#70)
    * chore: autoformat
    
    * feat: display fetch error
    
    * feat: add statusText property

--- explorer/lib/fetcher.ts
+++ explorer/lib/fetcher.ts
@@ -1,9 +1,27 @@
-const base_url = 'https://did.dyne.org';
+const base_url = "https://did.dyne.org";
 
 export const getData = async (endpoint: string) => {
-    const res = await fetch(`${base_url}/${endpoint}`);
-    if (!res.ok) {
-        throw new Error(`Failed to fetch data from ${endpoint}`);
-    }
-    return res.json();
+  const res = await fetch(`${base_url}/${endpoint}`);
+  if (!res.ok) {
+    throw new FetchError(
+      res.status,
+      `Failed to fetch data from ${endpoint}`,
+      res.statusText
+    );
+  }
+  return res.json();
+};
+
+export class FetchError extends Error {
+  status: number;
+  message: string;
+  statusText: string;
+
+  constructor(status: number, message: string, statusText: string) {
+    super(message);
+    this.name = "FetchError";
+    this.status = status;
+    this.message = message;
+    this.statusText = statusText;
+  }
 }
